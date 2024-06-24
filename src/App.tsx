79eaf7fb8035a41d7d08f088ed8b32b716a8e099
GABOCHEK <akramovartur58@gmail.com>
2024-06-24 14:22:09 +0400

    second version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { ProductFunctions } from './assets/Prod1'
-import { products } from './data/products'
+//import { products } from './data/products'
+import axios from 'axios'
+import { IProduct } from './models'
 
 function App() {
 
+    const [products, setProducts] = useState<IProduct[]>([])
+    async function fetchProducts() {
+        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products')
+        setProducts(response.data)
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
     return (
         <div className="container mx-auto max-w-2xl pt-5">
             {products.map(product => <ProductFunctions product={product} key={product.id} />)}
